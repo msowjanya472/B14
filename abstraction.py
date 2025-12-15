b09beff04dd318bc1942b2b8210a36f9b6fed02d
msowjanya472 <msowjanya472@gmail.com>
2025-12-15 16:12:52 +0530

    Implement word processing functions in abstraction.py
    Add functions to get word count and filter words by initial letter.

--- abstraction.py
+++ abstraction.py
@@ -1,4 +1,14 @@
+get_no_of_words()    
+print(data.split())
+def get_word_by_initial(letter):
+    words = data.split()
+    for wd in words:
+        if wd.startswith(letter.lower()):
+            print(len(wd))
+ 
+    print([wd for wd in words if wd.startswith(letter.lower())])
+ 
 
 
 
-https://github.com/msowjanya472/B14.git
+
