a2f5444097a6ed60f36b51ec4e8e14669c92405f
msowjanya472 <msowjanya472@gmail.com>
2025-12-15 16:13:49 +0530

    Refactor file handling and add utility functions
    Refactored file handling code to remove comments and improve functionality. Added functions to count words, get words by initial letter, and map word lengths.

--- filehandling.py
+++ filehandling.py
@@ -1,71 +1,67 @@
 
-#FILE_PATH = r"c:\Users\Murali\New folder\Python\kritiksha.txt"
-#f = open(FILE_PATH, "r")
-##print(dir(f))
-##content = f.readlines()
-##print(len(content))
-#
-#data= f.read()
+FILE_PATH = r"c:\Users\Murali\New folder\Python\kritiksha.txt"
+f = open(FILE_PATH, "r")
+print(dir(f))
+content = f.readlines()
+print(len(content))
+
+data= f.read()
+
+def get_no_of_words(): 
+    print(len(data.split()))
+
+
+get_no_of_words()    
+print(data.split())
+def get_word_by_initial(letter):
+    words = data.split()
+    for wd in words:
+        if wd.startswith(letter.lower()):
+            print(len(wd))
  
-#def get_no_of_words(): 
-#    print(len(data.split()))
-
-
-#get_no_of_words()    
-# print(data.split())
-
-#def get_word_by_initial(letter):
-#    words = data.split()
-##    for wd in words:
-##        if wd.startswith(letter.lower()):
-##            print(len(wd))
-# 
-#    print([wd for wd in words if wd.startswith(letter.lower())])
-# 
-#
-#
-#
-#get_word_by_initial("a")
-
-#def get_word_len_map():
-#    words = data.split()
-#    d = {}
-#    for wd in words:
-#        d[wd] = len(wd)
-#    print(d)    
-#    
+    print([wd for wd in words if wd.startswith(letter.lower())])
+ 
 
 
 
-#def get_total_char():
-#d   print(len(data))#d#d#d
-#det_total_char#d
+get_word_by_initial("a")
+def get_word_len_map():
+    words = data.split()
+    d = {}
+    for wd in words:
+        d[wd] = len(wd)
+    print(d)    
+    
+
+
+
+def get_total_char():
+    print(len(data))#d#d#d
 
 
 
 
-#data = f.read()
-##data2 = f.readline(3)
-#print(data)
-#data= f.read()
-#print(data)
-#print(content)
+
+data = f.read()
+data2 = f.readline(3)
+print(data)
+data= f.read()
+print(data)
+print(content)
 
 
 
-#import re
-#
-## Input paragraph
-##para = "Hello!!! Welcome to Python's world... #1 programming language @2025."
-#
-## Remove special characters (keep only letters, numbers, and spaces)
-#data = re.sub(r'[^A-Za-z0-9\s]', '', para)
-#
-#print("Original Paragraph:")
-#print(data)
-#print("\nCleaned Paragraph:")
-#print(para)
-#
+import re
+
+
+para = "Hello!!! Welcome to Python's world... #1 programming language @2025."
+#Remove special characters (keep only letters, numbers, and spaces)
+data = re.sub(r'[^A-Za-z0-9\s]', '', para)
+
+print("Original Paragraph:")
+print(data)
+print("\nCleaned Paragraph:")
+print(para)
 
 import re
 FILE_PATH = r"c:\Users\Murali\New folder\Python\kritiksha.txt"
@@ -89,5 +85,3 @@ print("Cleaning completed. Check 'output.txt'.")
 
 
 
-#f= close("c:\Users\Murali\New folder\Python\kritiksha.txt")
-#f = append()
